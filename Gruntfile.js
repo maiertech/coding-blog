d4d05b1c6444ce7319898c4813802a3519d069d9
Thilo Maier <github@maier.asia>
2014-09-06 18:25:31 -0400

    Gruntfile.js: group tasks and task configs, avoid duplication of config values with global configs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,153 +1,159 @@
 module.exports = function (grunt) {
 
-  // load tasks
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jekyll');
-  grunt.loadNpmTasks('grunt-rsync');
-
-  // set file encoding
   grunt.file.defaultEncoding = 'utf8';
 
-  // task configurations
   grunt.initConfig({
+    bowerrc: grunt.file.readJSON('.bowerrc'),
 
-      browserSync: {
-        files: [
-          '**/*.html',
-          'css/*.css',
-          'js/**/*.js'
-        ],
-        options: {
-          watchTask: true,
-          server: {
-            baseDir: '_site'
-          }
-        }
-      },
+    branches: {
+      production: 'master',
+      test: 'test'
+    },
 
-      compass: {
-        options: {
-          config: 'config.rb'
-        },
-        uncompressed: {
-          options: {
-            outputStyle: 'expanded'
-          }
-        },
-        compressed: {
-          options: {
-            outputStyle: 'compressed'
-          }
-        }
-      },
+    dirs: {
+      build: '_site',
+      deploy: '_deploy'
+    },
 
-      copy: {
-        fastclick: {
-          files: {
-            '_site/js/fastclick.js': '_bower_components/fastclick/lib/fastclick.js'
-          }
-        },
-        foundation: {
-          files: {
-            '_site/js/foundation.js': '_bower_components/foundation/js/foundation.js',
-            '_site/js/foundation.offcanvas.js': '_bower_components/foundation/js/foundation/foundation.offcanvas.js',
-            '_site/js/foundation.topbar.js': '_bower_components/foundation/js/foundation/foundation.topbar.js'
-          }
-        },
-        modernizr: {
-          files: {
-            '_site/js/modernizr.js': '_bower_components/modernizr/modernizr.js'
-          }
-        },
-        jquery: {
-          files: {
-            '_site/js/jquery.js': '_bower_components/jquery/dist/jquery.js'
-          }
-        },
-        options: {
-          nonull: true
-        }
-      },
+    env: process.env,
 
-      deploy: {
-        prod: {
-          options: {
-            server: 'coding.maier.asia'
-          }
-        },
-        test: {
-          options: {
-            server: 'coding-test.maier.asia'
-          }
-        }
-      },
-
-      jekyll: {
-        options: {
-          config: '_config.yml',
-          bundleExec: true
-        },
-        noDrafts: {
-          options: {
-            drafts: false
-          }
-        },
-        drafts: {
-          options: {
-            drafts: true
-          }
-        }
-      },
-
-      rsync: {
-        options: {
-          args: ['-vv'],
-          compareMode: 'checksum',
-          src: '_site/',
-          ssh: true,
-          syncDest: true,
-          recursive: true
-        },
-        prod: {
-          options: {
-            host: 'coding.maier.asia',
-            dest: '~/coding.maier.asia'
-          }
-        },
-        test: {
-          options: {
-            host: 'coding-test.maier.asia',
-            dest: '~/coding-test.maier.asia'
-          }
-        }
-      },
-
-      watch: {
-        compass: {
-          files: '_scss/**/*.scss',
-          tasks: ['compass:uncompressed']
-        },
-        jekyll: {
-          files: [
-            'index.html',
-            '_drafts/**/*.html',
-            '_includes/**/*.html',
-            '_layouts/**/*.html',
-            '_posts/**/*.md',
-            'css/**/*.css',
-            '_config.yml',
-            '_data/**/*.yml'
-          ],
-          tasks: ['jekyll:drafts', 'copy']
-        }
-      }
-
+    servers: {
+      production: 'coding.maier.asia',
+      test: 'coding-test.maier.asia'
     }
-  );
+  });
 
+  // browser-sync
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.config('browserSync', {
+    files: [
+      '**/*.html',
+      'css/*.css',
+      'js/**/*.js'
+    ],
+    options: {
+      watchTask: true,
+      server: {
+        baseDir: '<%= dirs.build %>'
+      }
+    }
+  });
+
+  // contrib-compass
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.config('compass', {
+    options: {
+      config: 'config.rb'
+    },
+    uncompressed: {
+      options: {
+        outputStyle: 'expanded'
+      }
+    },
+    compressed: {
+      options: {
+        outputStyle: 'compressed'
+      }
+    }
+  });
+
+  // contrib-copy
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.config('copy', {
+    fastclick: {
+      files: {
+        '<%= dirs.build %>/js/fastclick.js': '<%= bowerrc.directory %>/fastclick/lib/fastclick.js'
+      }
+    },
+    foundation: {
+      files: {
+        '<%= dirs.build %>/js/foundation.js': '<%= bowerrc.directory %>/foundation/js/foundation.js',
+        '<%= dirs.build %>/js/foundation.offcanvas.js': '<%= bowerrc.directory %>/foundation/js/foundation/foundation.offcanvas.js',
+        '<%= dirs.build %>/js/foundation.topbar.js': '<%= bowerrc.directory %>/foundation/js/foundation/foundation.topbar.js'
+      }
+    },
+    modernizr: {
+      files: {
+        '<%= dirs.build %>/js/modernizr.js': '<%= bowerrc.directory %>/modernizr/modernizr.js'
+      }
+    },
+    jquery: {
+      files: {
+        '<%= dirs.build %>/js/jquery.js': '<%= bowerrc.directory %>/jquery/dist/jquery.js'
+      }
+    },
+    options: {
+      nonull: true
+    }
+  });
+
+  // contrib-watch
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.config('watch', {
+    compass: {
+      files: '_scss/**/*.scss',
+      tasks: ['compass:uncompressed']
+    },
+    jekyll: {
+      files: [
+        'index.html',
+        '_drafts/**/*.html',
+        '_includes/**/*.html',
+        '_layouts/**/*.html',
+        '_posts/**/*.md',
+        'css/**/*.css',
+        '_config.yml',
+        '_data/**/*.yml'
+      ],
+      tasks: ['jekyll:drafts', 'copy']
+    }
+  });
+
+  // jekyll
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.config('jekyll', {
+    options: {
+      config: '_config.yml',
+      bundleExec: true
+    },
+    noDrafts: {
+      options: {
+        drafts: false
+      }
+    },
+    drafts: {
+      options: {
+        drafts: true
+      }
+    }
+  });
+
+  // rsync
+  grunt.loadNpmTasks('grunt-rsync');
+  grunt.config('rsync', {
+    options: {
+      args: ['-vv'],
+      compareMode: 'checksum',
+      src: '<%= dirs.build %>/',
+      ssh: true,
+      syncDest: true,
+      recursive: true
+    },
+    prod: {
+      options: {
+        host: '<%= servers.production %>',
+        dest: '~/<%= servers.production %>'
+      }
+    },
+    test: {
+      options: {
+        host: '<%= servers.test %>',
+        dest: '~/<%= servers.test %>'
+      }
+    }
+  });
+
+  // deploy
   /**
    * Deploy task.
    * Requires SSH user to be injected via --user=<username>.
@@ -176,8 +182,8 @@ module.exports = function (grunt) {
 
     // Write SSH config
     grunt.file.write(sshConfigFile, sshConfig);
-    grunt.log.writeln('=> SSH config: \n---\n' + sshConfig + '\n---\nto SSH config file');
-    grunt.log.writeln('=> SSH config written to: ' + sshConfigFile);
+    grunt.log.writeln('=> SSH config: \n---\n' + sshConfig + '\n---');
+    grunt.log.writeln('=> written to: ' + sshConfigFile);
 
     // prod deployment
     if (this.target === 'prod') {
@@ -189,7 +195,20 @@ module.exports = function (grunt) {
     grunt.log.writeln('=> run [addAuth, rsync:test]');
     return grunt.task.run(['addAuth', 'rsync:test']);
   });
+  grunt.config('deploy', {
+    prod: {
+      options: {
+        server: '<%= servers.production %>'
+      }
+    },
+    test: {
+      options: {
+        server: '<%= servers.test %>'
+      }
+    }
+  });
 
+  // build
   /**
    * Build task.
    * Trigger build for 'prod', 'test' and by default for 'dev'.
@@ -212,17 +231,19 @@ module.exports = function (grunt) {
         'deploy:test'           // Deploy to test server
       ]);
     }
-
-    return grunt.task.run([
-      'compass:uncompressed', // Compass compile without compression
-      'jekyll:drafts',        // Jekyll build with drafts
-      'copy',                 // copy JavaScript files
-      'browserSync',          // Launch BrowserSync and watch build dir for changes
-      'watch'                 // Watch Jekyll dir and trigger Compass compile or Jekyll build when files change
-    ]);
+    else {
+      return grunt.task.run([
+        'compass:uncompressed', // Compass compile without compression
+        'jekyll:drafts',        // Jekyll build with drafts
+        'copy',                 // copy JavaScript files
+        'browserSync',          // Launch BrowserSync and watch build dir for changes
+        'watch'                 // Watch Jekyll dir and trigger Compass compile or Jekyll build when files change
+      ]);
+    }
 
   });
 
+  // ci (continuous integration)
   /**
    * This is the task that needs to be run on Travis CI.
    * Requires SSH user to be injected via --user=<username>.
@@ -232,20 +253,19 @@ module.exports = function (grunt) {
     // Check documentation for CI and TRAVIS_BRANCH env variables:
     // http://docs.travis-ci.com/user/ci-environment/#Environment-variables
 
-    if (typeof process.env.CI == 'undefined') {
+    if (typeof grunt.config('env.CI') == 'undefined') {
       grunt.fail.fatal('This task can be executed only on the CI server!');
     }
 
-    var branch = process.env.TRAVIS_BRANCH
-    if (typeof branch == 'undefined') {
+    if (typeof grunt.config('env.TRAVIS_BRANCH') == 'undefined') {
       grunt.fail.fatal('TRAVIS_BRANCH env variable not set!');
     }
 
-    if (branch === 'master') { // deploy branch 'master' to production server
+    if (grunt.config('env.TRAVIS_BRANCH') === grunt.config('branches.production')) { // deploy production branch
       grunt.log.writeln('=> run build:prod');
       return grunt.task.run(['build:prod']);
     }
-    else if (branch === 'test') { // deploy branch 'test' to test server
+    else if (grunt.config('env.TRAVIS_BRANCH') === grunt.config('branches.test')) { // deploy test branch
       grunt.log.writeln('=> run build:test');
       return grunt.task.run(['build:test']);
     }
@@ -254,6 +274,7 @@ module.exports = function (grunt) {
     }
   });
 
+  // addAuth
   /**
    * This tasks replaces the root .htaccess file to add authentication.
    * This task is called prior to test deployment.
@@ -268,12 +289,15 @@ module.exports = function (grunt) {
 
     // overwrite .htaccess in build dir
     var replace = {
-      authName: 'coding.maier.asia (Test Server)',
+      authName: grunt.config('servers.production') + ' (Test Server)',
       user: user
     }
-    htaccess = grunt.template.process(grunt.file.read('_deploy/.htaccess'), {data: replace});
-    grunt.file.write('_site/.htaccess', htaccess);
+    htaccess = grunt.template.process(grunt.file.read(grunt.config('dirs.deploy') + '/.htaccess'), {data: replace});
+    grunt.file.write(grunt.config('dirs.build') + '/.htaccess', htaccess);
     grunt.log.writeln('=> replaced .htaccess file for test server deployment')
   });
+
+  // default
+  grunt.registerTask('default', ['build']);
 
 }
