4b896b6773329e2e4e828fab30f472a48acab4cf
Thilo Maier <git@maier.asia>
2020-12-29 05:09:24 +0000

    Upgrade @maiertech/gatsby-theme-digital-garden to v0.4.0 Fixes #335

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,16 @@ const pkg = require('./package.json');
 module.exports = {
   siteMetadata: {
     siteTitle: 'coding.maier.dev',
+    seoTitle: "Thilo Maier's digital garden on web development",
     siteAuthor: 'Thilo Maier',
-    description: pkg.description,
+    siteDescription: pkg.description,
     siteUrl: pkg.homepage,
     siteTwitter: '454de6e',
+    siteNavLinks: [
+      { href: '/posts/', text: 'Posts' },
+      { href: '/notes/', text: 'Notes' },
+      { href: '/about/', text: 'About' },
+    ],
   },
   plugins: ['@maiertech/gatsby-theme-digital-garden'],
 };
